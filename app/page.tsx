8a3167173ed6d5d49a7121a092d750d2bacfa1e9
Sumögåsbord <61753952+Sumogasbord@users.noreply.github.com>
2023-06-15 00:10:20 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -43,14 +43,10 @@ const RootPage = () => (
     <div className={styles.container}>
       <div>
         <h2>Welcome!</h2>
-        <p>Your easy-notion-blog deployed successfully!</p>
-        <p>Have fun!</p>
-        <p>
-          easy-notion-blog powered by{' '}
-          <Link href="https://github.com/otoyo/easy-notion-blog">
-            otoyo/easy-notion-blog
-          </Link>
+        <p>「いんたーねっつ★丁稚」である “Sumögåsbord” の実験用ウェブページです。<br/>
+        (ちなみにこのウェブサイトはNotionを活用しています。)
         </p>
+        <p>ただいま工事中です。完成までしばらくお待ちください、</p>
       </div>
     </div>
   </>
